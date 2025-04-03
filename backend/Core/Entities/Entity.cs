53e5c62f436fbbb7cbf4c202ad356929e80c7d34
Alex Valle <alexvalle75@gmail.com>
2025-04-03 20:38:21 +0200

    Created abstract Entity class

---
+++ backend/Core/Entities/Entity.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Backend.Core.Entities;
+
+public abstract class Entity<TId, TIdValue>
+    where TId : AbstractId<TIdValue>
+    where TIdValue : IEquatable<TIdValue>
+{
+    public TId Id { get; protected set; }
+
+    protected Entity(TId id) => Id = id;
+
+    /// <summary>
+    /// Check if two entities are equals.
+    /// Two entities are equals if their ids are the same.
+    /// </summary>
+    /// <param name="obj">The entity to equals to.</param>
+    /// <returns>True if the ids are the same, false otherwise</returns>
+    public override bool Equals(object? obj)
+    {
+        // Two entities are the same if they have the same id
+        if (obj is not Entity<TId, TIdValue> objEntity)
+        {
+            return false;
+        }
+
+        return objEntity.Id.Equals(Id);
+    }
+
+    public override int GetHashCode() => Id.GetHashCode();
+
+    public override string? ToString() => base.ToString();
+}
